8ebb5f4302b20add6c802e9dc494d2d859dceaa3
Lentil770 <mushkielent@gmail.com>
2020-08-07 13:55:18 +0100

    on of updated app with google search and current bestsellers s Please enter the commit message for your changes. Lines starting

--- index.js
+++ index.js
@@ -1,16 +1,4 @@
-'use strict';
-//to do wednesday: fix code issues(brings wrong list???????)done!, finish rendering done for one api!, fetch other api,
-//render other api,  finish js! ask opinions, do css!
-
-///https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key= latest bestseller list of books
-
-//https://cors-anywhere.herokuapp.com/ ******
-//to enable cors... pu tthis somewher, where?
-//jQuery.ajaxPrefilter(function(options) {
-//    if (options.crossDomain && jQuery.support.cors) {
-//        options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
-//    }
-//});
+'use strict'; 
 
 function bookBuyLink(bookTitle) {
     let bookSearch =  encodeURIComponent(bookTitle);
@@ -26,8 +14,8 @@ function renderPage(responseJson) {
             `<li class='result-li'>
             <h3 class='book-title'><img src='https://www.netclipart.com/pp/m/175-1753052_barnetts-reading-book-icon-flat-design.png' alt='book vector' class='book-vector'>${responseJson.Similar.Results[i].Name}</h3>
             <p class='book-teaser'>${responseJson.Similar.Results[i].wTeaser}</p>
-            <a href='${responseJson.Similar.Results[i].wUrl}' target='_blank'><button>learn more on wiki</button></a>
-            <a href='${bookBuyLink(responseJson.Similar.Results[i].Name)}' target='_blank'><button>buy it here!</button></a>
+            <a class='book-wiki' href='${responseJson.Similar.Results[i].wUrl}' target='_blank'><button>learn more on wiki</button></a>
+            <a class='book-buy' href='${bookBuyLink(responseJson.Similar.Results[i].Name)}' target='_blank'><button>buy it here!</button></a>
             
             </li>`
         );
@@ -55,13 +43,25 @@ buyLink .description, .pageCount, mainCategory/ categories, authors[can be range
 
 
 
-function fetchFunc() {
-    let tastediveRequest = 'https://cors-anywhere.herokuapp.com/' +  formatTasteDiveRequest();
+function formatTasteDiveRequest(searchID) { //need to fix it for using bookID from google resul button
+    console.log('formattastediverequest running');
+    const apiKey = '352618-ChayaLen-VGCEXWH6'
+    const bookID = searchID;
+    const tastedive = 'https://tastedive.com/api/similar?limit=10&info=1&type=books&q=book:';
+    //let bookIDReady = encodeURIComponent(bookID);
+    let tastediveRequest = tastedive + bookID + '&k=' + apiKey;
     console.log(tastediveRequest);
-    fetch(tastediveRequest) /*,  {
+    return tastediveRequest;
+}
+
+function fetchTasteDiveFunc(searchID) {
+    console.log('fetchtastediverunning', searchID);
+    let tastediveRequest = 'https://cors-anywhere.herokuapp.com/' +  formatTasteDiveRequest(searchID);
+    console.log('tastediverequest=', tastediveRequest);
+    fetch(tastediveRequest, {
         headers: new Headers({
-            'Access-Control-Allow-Origin': 'file:///C:/Users/mushk/coding/bloc/API-Hack-Capstone/index.html?' })
-    }) */
+            'Origin': 'file:///C:/Users/mushk/coding/bloc/API-Hack-Capstone/index.html?' })
+    } )
     .then (response => {
         if (response.ok) {
             return response.json();
@@ -73,54 +73,135 @@ function fetchFunc() {
             console.log('second .then', responseJson);
             renderPage(responseJson)
         })
-    .catch(err =>
-        {$('.js-fail-page').removeClass('hidden');
+    .catch(err => {
+        $('.js-fail-page').removeClass('hidden');
         $('.js-error').text(err);
         console.log(err)
+    }) 
+}
+
+function renderSearchResults(responseJson) {
+    //in here basically render button of each book title and author, and message telling users to select which one they want recommendations for.
+    //use renderPAge() for inspiration etc
+    // button onClick call fetchTastedivefunc(searchbookid)
+    console.log('rendersearchresults is running', responseJson);
+    $('.js-results-ul').html('');
+    $('.js-results-select').removeClass('hidden');
+    for (let i = 0;i < responseJson.items.length && 10 ;i++) {
+        const { volumeInfo } = responseJson.items[i];
+        $('.js-results-ul').append(
+            `<li class='search-result-li' >
+                <img src=${volumeInfo.imageLinks.smallThumbnail} alt='book vector' class='book-vector'>
+                <p class='book-title'>${volumeInfo.title}</p>
+                <span>by</span>
+                <p class='book-author'>${volumeInfo.authors.map(author => author + '\n')} <br/>
+                <p class='description'>${volumeInfo.description}</p>
+                <a class='book-wiki' href=${volumeInfo.infoLink} target='_blank' ><button class='book-ul-btn' >learn more about this book</button></a>
+                <a class='book-buy' href=${bookBuyLink(volumeInfo.title)} target='_blank' ><button class='book-ul-btn' >buy this book here</button></a> <br/>
+                <button class='recommend-btn ${volumeInfo.title} book-ul-btn ' value=${encodeURIComponent(volumeInfo.title)}><h3>more books like this</h3></button>
+            </li>`
+        );
+        $('.js-results').removeClass('hidden');
+    }
+    
+    $('.recommend-btn').click(function() {
+        console.log('recommended nutton clicked, this=:', $(this).val() );
+        fetchTasteDiveFunc($(this).val()) 
+    })
+} //works except: fix when under ten results doesnt raise error!? tried changing i< response.length...
+
+
+
+
+function fetchSearchFunc() {
+    let googleApiKey = 'AIzaSyDkjbMzgLOjujChHmKF1DnoKa3KhEaZsbc';
+    const userSearchTerm = $('.js-book-input').val(); //works till here
+    let googlebooksRequest = 'https://www.googleapis.com/books/v1/volumes?q=' + encodeURIComponent(userSearchTerm) + '&key=' + googleApiKey;
+    console.log('googlebooksrequest', googlebooksRequest);
+
+    fetch(googlebooksRequest) 
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } throw new Error(response.statusMessage)
+    })
+    .then(
+        responseJson => {
+            console.log('successfully got google books responsejson');
+            renderSearchResults(responseJson)
+        }
+    ).catch(err => {
+        $('.js-fail-page').removeClass('hidden');
+        $('.js-error').text(err);
+        console.log('fetchsearchfunc error:', err)
     })
 }
 
 
-function formatTasteDiveRequest() { //do for other api!!!      i took out: 
-    //format each of the requests and save to variables
-    const apiKey = '352618-ThumbsUp-8LNCY7RC'
-    const userInput = $('.js-book-input').val();
-    const tastedive = 'https://tastedive.com/api/similar?limit=10&info=1&type=books&q=book:';
-    let userInputReady = encodeURIComponent(userInput);
-    console.log(userInput, userInputReady, '= user input ready ')
-    let tastediveRequest = tastedive + userInputReady + '&k=' + apiKey;
-    console.log(tastediveRequest);
-    return tastediveRequest;
-}
+function renderCurrentBestsellerResults(responseJson) {
+    //in here basically render button of each book title and author, and message telling users to select which one they want recommendations for.
+    //use renderPAge() for inspiration etc
+    // button onClick call fetchTastedivefunc(searchbookid)
+    console.log('rendersearchresults is running', responseJson);
+    $('.js-results-ul').html('');
+    $('.js-results-select').removeClass('hidden');
+    for (let i = 0;i < responseJson.num_results ;i++) {
+        const { books } = responseJson.results;
+        $('.js-results-ul ').append(
+            `<li class='search-result-li' >
+                <img src=${books[i].book_image} alt='book vector' class='book-vector'>
+                <p class='book-title'>${books[i].title}</p>
+                <span>by</span>
+                <p class='book-author'>${books[i].author} <br/>
+                <p class='description'>${books[i].description}</p>
+                <a class='book-wiki' href=${books[i].amazon_product_url} target='_blank' ><button class='book-ul-btn'>learn more about this book</button></a>
+                <a class='book-buy' href=${bookBuyLink(books[i].title)} target='_blank' ><button class='book-ul-btn'>buy this book here</button></a> <br/>
+                <button class='recommend-btn ${books[i].title} book-ul-btn' value=${encodeURIComponent(books[i].title)}><h3>more books like this</h3></button>
+            </li>`
+        );
+        $('.js-results').removeClass('hidden');
+    }
+    
+    $('.recommend-btn').click(function() {
+        console.log('recommended nutton clicked, this=:', $(this).val() );
+        fetchTasteDiveFunc($(this).val()) 
+    })
+} //works except: fix when under ten results doesnt raise error!? tried changing i< response.length...
 
 function fetchCurrent() {
+    console.log('fetchcurrent function, here i want to fetch current bestsellers (eg) and on page load display them as recommended books');
+    fetch('https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=Qgs1AcncGyAGofxS7FMaXJ1ZI3GCTpB0')
+    .then(response => {
+        if (response.ok) {
+            return response.json();
+        } throw new Error(response.statusMessage)
+    })
+    .then(
+        responseJson => {
+            console.log('successfully got nytimes bestseller responsejson');
+            renderCurrentBestsellerResults(responseJson)
+        }
+    ).catch(err => {
+        $('.js-fail-page').removeClass('hidden');
+        $('.js-error').text(err);
+        console.log('fetchsearchfunc error:', err)
+    })
 }
-// https://www.goodreads.com/book/title.FORMAT  for book reviews
-
-    /* i need to take the title from fetchfunc, 
-    then insert that to new request call
-    , then fetch it, 
-    then insert it into button in html
-
-    */
-//or google books api has avg review and numreviews and description,  book img and buy link:)
 
 function watchpage() {
- // make  fetchCurrent(); on start page have recommendations for current bestsellers.
- //https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json?api-key=Qgs1AcncGyAGofxS7FMaXJ1ZI3GCTpB0
     fetchCurrent(); 
-    $('.js-input').on('submit', function(event) {
+    $('.js-input').on('submit', event => {
+        console.log('jsinput submitted');
         event.preventDefault();
-        fetchFunc();
-        console.log('watchpage ran', console.log($('body')));
-    })
-
+        fetchSearchFunc();
+        console.log('watchpage finished');
+    });
     /*
      when submit clicked,
      event.preventdefault
     format api requests (all of them )
-    fetch (all of them) (function which will fetch all oof them)   */
-}
+    fetch (all of them) (function which will fetch all of them)   */
+};
 
 
-$(watchpage())
+$(watchpage());
